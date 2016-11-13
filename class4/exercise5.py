34ea6d26c91303a63f4fdad02a575ec6888bc888
mreyes <mark.reyes@twcable.com>
2016-11-13 16:22:51 -0600

    handoff to pynet aws

--- class4/exercise5.py
+++ class4/exercise5.py
@@ -3,5 +3,39 @@
 5. Use Netmiko to enter into configuration mode on pynet-rtr2. Also use Netmiko to
 verify your state (i.e. that you are currently in configuration mode).
 """
-import paramiko
+from netmiko import ConnectHandler
+from getpass import getpass
 
+pynet1 = {
+	'device_type': 'cisco_ios',
+	'ip': '184.105.247.70',
+	'username': 'pyclass',
+	'password': '88newclass',
+	'port': 22,
+	}
+
+pynet2 = {
+	'device_type': 'cisco_ios',
+	'ip': '184.105.247.71',
+	'username': 'pyclass',
+	'password': '88newclass',
+	'port': 22,
+	}
+
+srx = {
+	'device_type': 'juniper',
+	'ip': '184.105.247.76',
+	'username': 'pyclass',
+	'password': '88newclass',
+	'secret': '',
+	'port': 22,
+	}
+
+print 'Connect to pynet1'
+pynet_rtr1 = ConnectHandler(**pynet1)
+
+print 'enter config mode'
+pynet_rtr1.config_mode()
+
+print 'am i in config mode?'
+print pynet_rtr1.check_config_mode()
--- class4/exercise6.py
+++ class4/exercise6.py
@@ -1,6 +1,40 @@
 #!/usr/bin/env python
 """
-6. Use Netmiko to execute 'show arp' on pynet-rtr1, pynet-rtr2, and juniper-srx.
+6. Use Netmiko to execute 'show arp' on pynet-rtr1, pynet-rtr2,
+and juniper-srx.
 """
-import paramiko
+from netmiko import ConnectHandler
+from getpass import getpass
 
+pynet1 = {
+    'device_type': 'cisco_ios',
+    'ip': '184.105.247.70',
+    'username': 'pyclass',
+    'password': '88newclass',
+    'port': 22,
+    }
+
+pynet2 = {
+    'device_type': 'cisco_ios',
+    'ip': '184.105.247.71',
+    'username': 'pyclass',
+    'password': '88newclass',
+    'port': 22,
+    }
+
+srx = {
+    'device_type': 'juniper',
+    'ip': '184.105.247.76',
+    'username': 'pyclass',
+    'password': '88newclass',
+    'secret': '',
+    'port': 22,
+    }
+
+dev_list = [ 'pynet1', 'srx' ]
+
+for device in (pynet1, pynet2):
+    print device
+    ssh = ConnectHandler('**device')
+    # output = ssh.find_prompt()
+    # print output
