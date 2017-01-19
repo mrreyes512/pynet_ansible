c1bde2b37d8c732d4f22344451c1d324c02eda80
mreyes <mark.reyes@twcable.com>
2017-01-19 00:34:49 -0600

    Complete up to ex4

---
+++ class9/exercise3/mytest/whatever.py
@@ -0,0 +1,6 @@
+
+def func3():
+    print "This is whatever"
+
+if __name__ == "__main__":
+        print "This is main task for whatever.py"
---
+++ class9/exercise3/mytest/world.py
@@ -0,0 +1,6 @@
+
+def func2():
+    print "Hello Worldy World!"
+
+if __name__ == "__main__":
+        print "This is main task for world.py"
